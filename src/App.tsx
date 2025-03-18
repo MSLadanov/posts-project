7dc5d8a24e18feea0f26807c5bdda8b4aaae336c
MSLadanov <MSLadanov@gmail.com>
2025-03-18 15:01:41 +0300

    chore: :wrench: setup path aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import "./App.css";
-import { Header } from "@/components/Header";
-import { TodoList } from "@/components/TodoList";
-import { Todos } from "@/components/Todos";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <TodoList />
-      <Todos />
     </div>
   );
 }
---
+++ src/components/Todo/Todo.tsx
@@ -0,0 +1,5 @@
+import { ReactElement } from "react"
+
+export const Todo = () : ReactElement => {
+    return <div>Todo</div>
+}
