8551f5301ecb591581662ff1b29ec96d65d44fff
MSLadanov <MSLadanov@gmail.com>
2025-03-18 15:18:02 +0300

    chore: :construction: creating base routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { TodoList } from "./components/TodoList";
+import { Todos } from "./components/Todos";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="todolists" element={<TodoList />} />
+          <Route path="todos" element={<Todos />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
