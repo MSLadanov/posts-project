78f9c5f7d9ece984f786d17dd68566242aee9b10
MSLadanov <MSLadanov@gmail.com>
2025-03-19 10:23:43 +0300

    chore: :art: improve routing

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "@/App";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import { TodoListsRoute } from "@routes/TodoListsRoute";
 import { TodosRoute } from "@routes/TodosRoute";
-
+import { ErrorRoute } from "@routes/ErrorRoute";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,9 +14,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<ErrorRoute />} />
         <Route path="/" element={<App />}>
           <Route path="todolists" element={<TodoListsRoute />} />
           <Route path="todos" element={<TodosRoute />} />
+          <Route path="/" element={<Navigate to="/todolists" replace />} />
         </Route>
       </Routes>
     </BrowserRouter>
