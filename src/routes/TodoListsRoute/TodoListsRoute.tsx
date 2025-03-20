c109751f5ec29d64920af21aa118723023bd6783
MSLadanov <MSLadanov@gmail.com>
2025-03-20 10:22:28 +0300

    chore: :construction: update todolists route

--- src/routes/TodoListsRoute/TodoListsRoute.tsx
+++ src/routes/TodoListsRoute/TodoListsRoute.tsx
@@ -2,7 +2,15 @@ import useFetch from "@/hooks/useFetch";
 import { ReactElement } from "react";
 
 export const TodoListsRoute = (): ReactElement => {
-  const {data, isLoading, isError} = useFetch("data.json");
-  console.log('render', {data, isLoading, isError})
+  const { data, isLoading, isError } = useFetch("data.json");
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error!</div>;
+  }
+  if (!isLoading && !isError && data.length === 0) {
+    return <div>No TodoLists!</div>;
+  }
   return <div>TodoListsRoute</div>;
 };
