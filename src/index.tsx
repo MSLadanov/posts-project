7b57e3dc0b5ebbeec60fc5dfa6fb779fe93ded14
MSLadanov <MSLadanov@gmail.com>
2025-03-21 09:57:12 +0300

    chore: :arrow_up: implemented redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { App } from "@/App";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import { App } from "@/App";
 import { TodoListsRoute } from "@routes/TodoListsRoute";
 import { TodosRoute } from "@routes/TodosRoute";
 import { ErrorRoute } from "@routes/ErrorRoute";
@@ -12,15 +14,17 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<ErrorRoute />} />
-        <Route path="/" element={<App />}>
-          <Route path="todolists" element={<TodoListsRoute />} />
-          <Route path="todolists/:id" element={<TodosRoute />} />
-          <Route path="/" element={<Navigate to="/todolists" replace />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<ErrorRoute />} />
+          <Route path="/" element={<App />}>
+            <Route path="todolists" element={<TodoListsRoute />} />
+            <Route path="todolists/:id" element={<TodosRoute />} />
+            <Route path="/" element={<Navigate to="/todolists" replace />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/store/slices/TodoSlice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const todoReducer = createSlice({
+    name: 'todo',
+    initialState: [],
+    reducers: {
+        
+    }
+})
+
+export default todoReducer.reducer
