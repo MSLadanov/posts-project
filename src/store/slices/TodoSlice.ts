56d40263a4715be3a8be76f19cbd7a556456b1d2
MSLadanov <MSLadanov@gmail.com>
2025-03-21 18:52:40 +0300

    chore: :construction: configuring store

--- src/store/slices/TodoSlice.ts
+++ src/store/slices/TodoSlice.ts
@@ -1,13 +1,47 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { TTodoList } from "@/types/todoTypes";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const fetchTodos = createAsyncThunk('todos/fetchTodos',
+    async () => {
+        const response = await fetch('data.json');
+        const { todoLists } = await response.json();
+        return todoLists;
+    }
+);
+
+interface TodoListsState {
+    todoLists: TTodoList[];
+    loading: 'idle' | 'pending' | 'succeeded' | 'failed';
+}
+
+const initialState: TodoListsState = {
+    todoLists: [],
+    loading: 'idle',
+};
 
 const todoReducer = createSlice({
-    name: 'todo',
-    initialState: [],
+    name: 'todoLists',
+    initialState,
     reducers: {
+        
         getTodo: (state, action) => {
-            state.push(action.payload)
+         
         }
-    }
-})
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchTodos.pending, (state) => {
+                state.loading = 'pending';
+            })
+            .addCase(fetchTodos.fulfilled, (state, action) => {
+                state.loading = 'succeeded';
+                state.todoLists = action.payload; 
+            })
+            .addCase(fetchTodos.rejected, (state) => {
+                state.loading = 'failed';
+            });
+    },
+});
 
-export default todoReducer.reducer
+export const { getTodo } = todoReducer.actions;
+export default todoReducer.reducer;
