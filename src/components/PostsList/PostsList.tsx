cf5e2266f22f2abb11ad0d0f912167eba86468f7
MSLadanov <MSLadanov@gmail.com>
2025-03-30 23:21:42 +0300

    chore: :construction: creating post list page

--- src/components/PostsList/PostsList.tsx
+++ src/components/PostsList/PostsList.tsx
@@ -10,6 +10,7 @@ export const PostsList: React.FC<IPostsListProps> = ({
   id,
   body,
 }): ReactElement => {
+  console.log(id)
   const location = useLocation();
   return <NavLink to={`${location.pathname}${id}`}>{body}</NavLink>;
 };
--- src/routes/PostsListRoute/PostsListRoute.tsx
+++ src/routes/PostsListRoute/PostsListRoute.tsx
@@ -1,8 +1,9 @@
 import { fetchPosts } from "@/store/slices/PostsSlice";
-import { TPostsListStore } from "@/types/types";
+import { TPost, TPostsListStore } from "@/types/types";
 import { ReactElement, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "@/store";
+import { PostsList } from "@/components/PostsList";
 
 export const PostsListRoute = (): ReactElement => {
   const dispatch = useDispatch<AppDispatch>();
@@ -17,9 +18,9 @@ export const PostsListRoute = (): ReactElement => {
   }
   return (
     <div>
-      {/* {data.map((item) => (
-        <PostsList key={item.id} id={item.id} title={item.title} />
-      ))} */}
+      {data.map((item : TPost) => (
+        <PostsList key={item.id} id={item.id} body={item.title} />
+      ))}
     </div>
   );
 };
