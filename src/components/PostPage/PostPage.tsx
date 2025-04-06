6b8446afb0a0d768ad3595116f339e3d0e817be3
MSLadanov <MSLadanov@gmail.com>
2025-04-06 22:37:08 +0300

    chore: :construction: creating post page

--- src/components/PostPage/PostPage.tsx
+++ src/components/PostPage/PostPage.tsx
@@ -3,14 +3,19 @@ import { useLocation } from "react-router";
 import { fetchPostById } from "@/store/slices/PostsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "@/store";
+import { TPostsListStore } from "@/types/types";
+import "./style.scss";
 
 export const PostPage = (): ReactElement => {
   const { pathname } = useLocation();
   const postId = Number(pathname.split("/").at(-1));
   const dispatch = useDispatch<AppDispatch>();
-  const {} = useSelector()
+  const { data, loading } = useSelector(
+    (store: TPostsListStore) => store.posts.post
+  );
   useEffect(() => {
     dispatch(fetchPostById(postId));
+    console.log(data);
   }, [dispatch, postId]);
   return <div>Post Page</div>;
 };
