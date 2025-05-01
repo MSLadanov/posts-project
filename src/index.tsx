0d85cb68433c215af8da2613c4f0aadcdd627920
MSLadanov <MSLadanov@gmail.com>
2025-05-01 19:04:52 +0300

    chore: :construction: creating error component

--- src/index.tsx
+++ src/index.tsx
@@ -8,23 +8,26 @@ import { App } from "@/App";
 import { PostsListRoute } from "@/routes/PostsListRoute";
 import { PostRoute } from "@/routes/PostRoute";
 import { ErrorRoute } from "@routes/ErrorRoute";
+import { ErrorBoundary } from "react-error-boundary";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<ErrorRoute />} />
-          <Route path="/" element={<App />}>
-            <Route path="posts" element={<PostsListRoute />} />
-            <Route path="posts/:id" element={<PostRoute />} />
-            <Route path="/" element={<Navigate to="/posts" replace />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="*" element={<ErrorRoute />} />
+            <Route path="/" element={<App />}>
+              <Route path="posts" element={<PostsListRoute />} />
+              <Route path="posts/:id" element={<PostRoute />} />
+              <Route path="/" element={<Navigate to="/posts" replace />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
