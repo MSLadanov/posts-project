6540923aeea2bfea063c7a2ae17ac95a3f629a2f
MSLadanov <MSLadanov@gmail.com>
2025-05-06 22:45:15 +0300

    chore: :construction: configureing store

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -1,24 +1,15 @@
-import { ReactElement, useEffect } from "react";
+import { ReactElement } from "react";
 import useFetch from "@hooks/useFetch";
 import { TComment } from "@/types/types";
 import { Comment } from "@components/Comment";
 import { CommentInput } from "@components/CommentInput";
-import { Loader } from "@components/Loader";
 
 export const Comments: React.FC<{ postId: number }> = ({
   postId,
 }): ReactElement => {
-  const { data, isLoading, isError } = useFetch<{ comments: TComment[] }>(
+  const { data, } = useFetch<{ comments: TComment[] }>(
     `https://dummyjson.com/comments/post/${postId}`
   );
-  useEffect(() => {
-  }, [data]);
-  if (isLoading) {
-    return <Loader/> ;
-  }
-  if (isError) {
-    return <div>Ошибка</div>;
-  }
   return (
     <div>
       <CommentInput/>
