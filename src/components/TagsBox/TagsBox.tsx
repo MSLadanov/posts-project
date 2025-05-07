3ce268f5d4369d8bb34e7322d65d5c4bfeb7c946
MSLadanov <MSLadanov@gmail.com>
2025-05-07 23:17:46 +0300

    chore: :construction: creating sort by tags component

--- src/components/TagsBox/TagsBox.tsx
+++ src/components/TagsBox/TagsBox.tsx
@@ -1,9 +1,11 @@
 import { ReactElement, Suspense } from "react";
-import "./style.scss";
 import useFetch from "@hooks/useFetch";
 import { Tag } from "@/types/types";
+import { TagBadge } from "@ui/TagBadge";
+import "./style.scss";
+import { Loader } from "../Loader";
 
 export const TagsBox = (): ReactElement => {
   const { data } = useFetch<Tag[]>("https://dummyjson.com/posts/tags");
-  return <Suspense>{data.map()}</Suspense>;
+  return <Suspense fallback={<Loader/>}>{data!.map((tag) => <TagBadge>{tag.name}</TagBadge>)}</Suspense>;
 };
