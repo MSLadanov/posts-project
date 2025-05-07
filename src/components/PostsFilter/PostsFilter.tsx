f8a62587b03cbdedf1093f647143d0686d70b6df
MSLadanov <MSLadanov@gmail.com>
2025-05-07 23:05:40 +0300

    chore: :construction: creating sort by tags component

--- src/components/PostsFilter/PostsFilter.tsx
+++ src/components/PostsFilter/PostsFilter.tsx
@@ -1,12 +1,14 @@
 import { ReactElement } from "react";
 import { SearchBox } from "@components/SearchBox";
 import { SortBox } from "@components/SortBox";
+import { TagsBox } from "@components/TagsBox";
 
 export const PostsFilter = (): ReactElement => {
   return (
     <div className="posts-filter">
       <SearchBox />
       <SortBox />
+      <TagsBox/>
     </div>
   );
 };
---
+++ src/components/TagsBox/TagsBox.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from "react";
+import './style.scss'
+
+export const TagsBox = () : ReactElement => {
+    return <div>Tags</div>
+}
