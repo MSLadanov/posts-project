fdb79aa3bf0e06a3d2b0ae367bc3cae515d6f743
MSLadanov <MSLadanov@gmail.com>
2025-05-16 20:00:53 +0300

    chore: :construction: creating modal

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,27 +1,45 @@
-import { useEffect, useState } from "react";
+import { TPost, TUser } from "@/types/types";
+import { useState } from "react";
 import { useErrorBoundary } from "react-error-boundary";
 
-const useFetch = <T> (url: string) => {
-  const { showBoundary } = useErrorBoundary()
-  const [data, setData] = useState<T>();
+const useFetch = <T>(baseUrl: string) => {
+  const { showBoundary } = useErrorBoundary();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isError, setIsError] = useState<boolean>(false);
-  useEffect(() => {
-    (async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch(url);
-        const fetchedData = await response.json();
-        setData(fetchedData);
-      } catch (error) {
-        setIsError(true);
-        showBoundary(error)
-      } finally {
-        setIsLoading(false);
-      }
-    })();
-  }, [showBoundary, url]);
-  return { data, isLoading, isError };
+  const get = async (endpoing: string) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${baseUrl}/${endpoing}`);
+      const data: T = await response.json();
+      setIsLoading(false);
+      return data;
+    } catch (error) {
+      showBoundary(error);
+      setIsError(true);
+    }
+  };
+  const post = async (
+    endpoing: string,
+    body: Partial<TUser> | Partial<TPost>
+  ) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${baseUrl}/${endpoing}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(body),
+      });
+      const data: T = await response.json();
+      setIsLoading(false);
+      return data;
+    } catch (error) {
+      showBoundary(error);
+      setIsError(true);
+    }
+  };
+  return { get, post, isLoading, isError };
 };
 
 export default useFetch;
