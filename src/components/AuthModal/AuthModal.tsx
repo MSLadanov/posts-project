99b7b565ce91311b235562897debacd921cc44e4
MSLadanov <MSLadanov@gmail.com>
2025-05-17 13:06:44 +0300

    chore: :construction: creating modal

--- src/components/AuthModal/AuthModal.tsx
+++ src/components/AuthModal/AuthModal.tsx
@@ -14,15 +14,81 @@ const SignIn = (): ReactElement => {
   };
   return (
     <form onSubmit={(e) => handleSignInForm(e)}>
-      <Input type="text" value={userName} setValue={setUserName} />
-      <Input type="password" value={password} setValue={setPassword} />
+      <Input
+        type="text"
+        value={userName}
+        required={true}
+        label="Username:"
+        setValue={setUserName}
+      />
+      <Input
+        type="password"
+        value={password}
+        required={true}
+        label="Password:"
+        setValue={setPassword}
+      />
       <button type="submit">Sign In</button>
     </form>
   );
 };
 
 const SignUp = (): ReactElement => {
-  return <form>Sign Up</form>;
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [gender, setGender] = useState("male");
+  const handleSignUpForm = async (e: FormEvent) => {
+    e.preventDefault();
+    console.log({ userName, password, email, firstName, lastName, gender });
+  };
+  return (
+    <form onSubmit={(e) => handleSignUpForm(e)}>
+      <Input
+        type="text"
+        value={userName}
+        required={true}
+        label="Username:"
+        setValue={setUserName}
+      />
+      <Input
+        type="password"
+        value={password}
+        required={true}
+        label="Password:"
+        setValue={setPassword}
+      />
+      <Input
+        type="email"
+        value={email}
+        required={true}
+        label="E-mail:"
+        setValue={setEmail}
+      />
+      <Input
+        type="email"
+        value={firstName}
+        label="First name:"
+        setValue={setFirstName}
+      />
+      <Input
+        type="text"
+        value={lastName}
+        label="Last name"
+        setValue={setLastName}
+      />
+      <Input type="radio" value={"male"} label="male" setValue={setGender} />
+      <Input
+        type="radio"
+        value={"female"}
+        label="female"
+        setValue={setGender}
+      />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
 };
 
 export const AuthModal = (): ReactElement => {
