036bb21ae05d508512be3870399019eca8a3eb2b
MSLadanov <MSLadanov@gmail.com>
2025-05-20 21:29:01 +0300

    chore: :construction: creating post slice

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,20 @@
 import { ReactElement, useEffect } from "react";
 import { useCookies } from "react-cookie";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { getCurrentUser } from "@/store/slices/UserSlice";
 import { AppDispatch } from "@/store";
-import { TUserStore } from "@/types/types";
 import SignedHeader from "./SignedHeader";
+import { useCheckToken } from "@/hooks/useCheckToken";
 import BaseHeader from "./BaseHeader";
 
 export const Header = (): ReactElement => {
   const [cookies] = useCookies(["accessToken"]);
   const dispatch = useDispatch<AppDispatch>();
+  const { isLogged } = useCheckToken();
   useEffect(() => {
     if (cookies.accessToken) {
       dispatch(getCurrentUser(cookies.accessToken));
     }
   }, [cookies, dispatch]);
-  const data = useSelector((store: TUserStore) => store.user.data);
-  return <header>{data.id ? <SignedHeader /> : <BaseHeader />}</header>;
+  return <header>{isLogged ? <SignedHeader /> : <BaseHeader />}</header>;
 };
