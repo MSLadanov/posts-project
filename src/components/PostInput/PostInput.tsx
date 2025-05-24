f84081b351c4a9ebb806e8638899fb0a2c3cba70
MSLadanov <MSLadanov@gmail.com>
2025-05-24 11:57:22 +0300

    chore: :construction: creating add new post feature

--- src/components/PostInput/PostInput.tsx
+++ src/components/PostInput/PostInput.tsx
@@ -1,6 +1,22 @@
-import { ReactElement } from "react";
-import './style.scss'
+import { ReactElement, useState } from "react";
+import { TextArea } from "../ui/TextArea";
+import { Button } from "@ui/Button";
+import "./style.scss";
+import { Input } from "../ui/Input";
 
-export const PostInput = () : ReactElement => {
-    return <div>Post Input</div>
-}
+export const PostInput = (): ReactElement => {
+  const [postBody, setPostBody] = useState("");
+  const [postTitle, setPostTitle] = useState("");
+  return (
+    <div>
+      <Input
+        value={postTitle}
+        setValue={setPostTitle}
+        type="text"
+        label="Post title:"
+      />
+      <TextArea value={postBody} setValue={setPostBody} />
+      <Button />
+    </div>
+  );
+};
