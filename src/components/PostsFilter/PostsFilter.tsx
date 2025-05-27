84747c67a963d69176ced2e3bfe6e463e1e2708c
MSLadanov <MSLadanov@gmail.com>
2025-05-27 10:23:53 +0300

    chore: :bug: fix rerender bug

--- src/components/PostsFilter/PostsFilter.tsx
+++ src/components/PostsFilter/PostsFilter.tsx
@@ -1,26 +1,23 @@
-import React, { ReactElement, Suspense } from "react";
+import { ReactElement } from "react";
 import { SearchBox } from "@components/SearchBox";
 import { SortBox } from "@components/SortBox";
-import { Loader } from "../Loader";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "@/store";
 import { fetchPostsByTag } from "@/store/slices/PostsSlice";
 import { TPostsListStore } from "@/types/types";
+import TagsBox from "../TagsBox/TagsBox";
 
 export const PostsFilter = (): ReactElement => {
   const dispatch = useDispatch<AppDispatch>();
-  const TagsBox = React.lazy(() => import("@components/TagsBox/TagsBox"));
   const { tag } = useSelector((state: TPostsListStore) => state.posts);
   const handleSlug = (slug: string) => {
     dispatch(fetchPostsByTag(slug));
   };
   return (
     <div className="posts-filter">
-      <Suspense fallback={<Loader />}>
       <SearchBox />
       <SortBox />
-        <TagsBox getSlug={handleSlug} tagStore={tag} />
-      </Suspense>
+      <TagsBox getSlug={handleSlug} tagStore={tag} />
     </div>
   );
 };
--- src/routes/PostsListRoute/PostsListRoute.tsx
+++ src/routes/PostsListRoute/PostsListRoute.tsx
@@ -1,16 +1,12 @@
-import { ReactElement, Suspense } from "react";
+import { ReactElement } from "react";
 import { PostsFilter } from "@/components/PostsFilter";
-import React from "react";
-import { Loader } from "@/components/Loader";
+import PostsList from "@/components/PostsList/PostsList";
 
 export const PostsListRoute = (): ReactElement => {
-  const PostsList = React.lazy(() => import("@components/PostsList/PostsList"));
   return (
     <main>
       <PostsFilter />
-      <Suspense fallback={<Loader />}>
-        <PostsList />
-      </Suspense>
+      <PostsList />
     </main>
   );
 };
