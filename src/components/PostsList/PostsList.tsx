cf2dfe7b599be6b3a6e0545a81b06c65759f0db2
MSLadanov <MSLadanov@gmail.com>
2025-05-29 11:52:33 +0300

    chore: :construction: add styles

--- src/components/PostsList/PostsList.tsx
+++ src/components/PostsList/PostsList.tsx
@@ -1,11 +1,12 @@
 import { AppDispatch } from "@/store";
-import { fetchPosts, fetchTags } from "@/store/posts.api"
+import { fetchPosts, fetchTags } from "@/store/posts.api";
 import { TPostAppStore, TPost } from "@/types/types";
 import { ReactElement, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Post } from "@components/Post";
 import { PostInput } from "@components/PostInput";
 import { ProtectedComponent } from "@components/ProtectedComponent";
+import './style.scss'
 
 export const PostsList = (): ReactElement => {
   const dispatch = useDispatch<AppDispatch>();
@@ -24,9 +25,11 @@ export const PostsList = (): ReactElement => {
       <ProtectedComponent>
         <PostInput />
       </ProtectedComponent>
-      {data.map((item: TPost) => (
-        <Post key={item.id} post={item} />
-      ))}
+      <div className="post-list">
+        {data.map((item: TPost) => (
+          <Post key={item.id} post={item} />
+        ))}
+      </div>
     </>
   );
 };
--- src/components/ui/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/ui/Breadcrumbs/Breadcrumbs.tsx
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router";
 import useBreadcrumbs from "use-react-router-breadcrumbs";
+import './style.scss'
 
 export const Breadcrumbs = () => {
   const breadcrumbs = useBreadcrumbs([]);
   return (
-    <div>
+    <div className="breadcrumbs">
       {breadcrumbs.map(({ match, breadcrumb }) => (
         <NavLink key={match.pathname} to={match.pathname}>
           {breadcrumb}
