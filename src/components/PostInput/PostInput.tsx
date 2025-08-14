4613e11d35657f3c734d09d9f9ad12371d2cd4e4
MSLadanov <MSLadanov@gmail.com>
2025-08-14 17:17:27 +0300

    refactor: :recycle: refactor code, migrate to tanstack

--- src/components/PostInput/PostInput.tsx
+++ src/components/PostInput/PostInput.tsx
@@ -8,62 +8,20 @@ import {
   faUpLong,
 } from "@fortawesome/free-solid-svg-icons";
 import { TagsBox } from "@components/TagsBox";
-import { useSelector } from "react-redux";
-import { TPost, TPostAppStore } from "@/types/types";
-import { fetchImage } from "@/api/utils";
-import useFetch from "@/hooks/useFetch";
-import { useNotify } from "@/hooks/useNotify";
 import { useQuery } from "@tanstack/react-query";
 import { fetchTags } from "@/api/posts.api";
 import "./style.scss";
+import { useNewPost } from "@/hooks/useNewPost";
 
 export const PostInput = (): ReactElement => {
-  const { id, firstName, lastName, image } = useSelector(
-    (state: TPostAppStore) => state.user.data
-  );
-  const {data: newPostTags, isLoading, isError} = useQuery({queryKey:['new-post-tags'], queryFn: fetchTags})
+  const {
+    data: newPostTags,
+    isLoading,
+    isError,
+  } = useQuery({ queryKey: ["new-post-tags"], queryFn: fetchTags });
   const [isOpened, setIsOpened] = useState(false);
-  const [body, setBody] = useState("");
-  const [title, setTitle] = useState("");
-  const [tags, setTags] = useState<string[]>([]);
-  const clearPostInput = () => {
-    setBody("");
-    setTitle("");
-    setTags([]);
-    setIsOpened(false);
-  };
-  const { notify, notifyPortal } = useNotify();
-  const handleSlug = (slug: string) => {
-    setTags([...tags, slug]);
-  };
-  const { post } = useFetch(`https://dummyjson.com`);
-  const addNewPost = async () => {
-    const newPost: TPost = {
-      id: Math.floor(Math.random() * 20001) + 10000,
-      userId: id,
-      user: {
-        firstName,
-        lastName,
-        image,
-      },
-      comments: [],
-      rate: "",
-      postImage: await fetchImage(title),
-      rated: false,
-      title,
-      body,
-      tags,
-      views: 0,
-      reactions: {
-        likes: 0,
-        dislikes: 0,
-      },
-    };
-    console.log(newPost)
-    notify("New post added!", "success");
-    await post("posts/add", newPost);
-    clearPostInput();
-  };
+  const { title, body, addPost, notifyPortal, handleTag, setBody, setTitle } =
+    useNewPost();
   return (
     <div>
       <Button
@@ -75,7 +33,7 @@ export const PostInput = (): ReactElement => {
       </Button>
       <div className={isOpened ? "post-input__visible" : "post-input"}>
         <h4>Choose tags:</h4>
-        <TagsBox getSlug={handleSlug} tags={newPostTags} />
+        <TagsBox getSlug={handleTag} tags={newPostTags} />
         <Input
           value={title}
           setValue={setTitle}
@@ -84,12 +42,7 @@ export const PostInput = (): ReactElement => {
           name="post-title"
         />
         <TextArea value={body} setValue={setBody} label="Post text:" />
-        <Button
-          action={addNewPost}
-          payload={{ body, title, tags }}
-          icon={faPaperPlane}
-          style={{ color: "black" }}
-        >
+        <Button action={addPost} icon={faPaperPlane} style={{ color: "black" }}>
           Send post
         </Button>
       </div>
---
+++ src/hooks/useNewPost.ts
@@ -0,0 +1,93 @@
+import { TPost, TPostAppStore } from "@/types/types";
+import { ReactPortal, SetStateAction, useState } from "react";
+import { useSelector } from "react-redux";
+import useFetch from "./useFetch";
+import { fetchImage } from "@/api/utils";
+import { useNotify } from "./useNotify";
+import {
+  UseMutateFunction,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { Dispatch } from "react";
+import { fetchPosts } from "@/api/posts.api";
+
+interface IUseNewPostReturn {
+  body: string;
+  title: string;
+  tags: string[];
+  notifyPortal: ReactPortal | null | false;
+  setTitle: Dispatch<SetStateAction<string>>;
+  setBody: Dispatch<SetStateAction<string>>;
+  handleTag: (tag: string) => void;
+  clearPostInput: () => void;
+  addPost: UseMutateFunction;
+}
+
+export const useNewPost = (): IUseNewPostReturn => {
+  const {data: posts} = useQuery({queryKey:['posts'], queryFn: fetchPosts})
+  const queryClient = useQueryClient();
+  const { id, firstName, lastName, image } = useSelector(
+    (state: TPostAppStore) => state.user.data
+  );
+  const { post } = useFetch(`https://dummyjson.com`);
+  const [body, setBody] = useState("");
+  const [title, setTitle] = useState("");
+  const [tags, setTags] = useState<string[]>([]);
+  const { notify, notifyPortal } = useNotify();
+  const clearPostInput = () => {
+    setBody("");
+    setTitle("");
+    setTags([]);
+  };
+  const handleTag = (tag: string) => {
+    setTags([...tags, tag]);
+  };
+  const addNewPost = async () => {
+    const newPostData: TPost = {
+      id: 0,
+      userId: id,
+      user: {
+        firstName,
+        lastName,
+        image,
+      },
+      comments: [],
+      rate: "",
+      postImage: await fetchImage(title),
+      rated: false,
+      title,
+      body,
+      tags,
+      views: 0,
+      reactions: {
+        likes: 0,
+        dislikes: 0,
+      },
+    };
+    const response = await post<TPost>("posts/add", newPostData);
+    const newPost = {...newPostData, id: response?.id}
+    clearPostInput();
+    return newPost
+  };
+  const { mutate: addPost } = useMutation({
+    mutationKey: ["posts"],
+    mutationFn: addNewPost,
+    onSuccess:(newPost) => {
+      queryClient.setQueryData(['posts'], [newPost, posts])
+      notify("New post added!", "success");
+    }
+  });
+  return {
+    body,
+    title,
+    tags,
+    notifyPortal,
+    setTitle,
+    setBody,
+    handleTag,
+    clearPostInput,
+    addPost,
+  };
+};
